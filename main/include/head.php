be7bc6d2cf7fa3bf9c416fa03cce3543b0c87cf0
new-crystal <oj3339@naver.com>
2023-11-15 09:58:25 +0900

    edit index.view, header, footer main page

--- main/include/head.php
+++ main/include/head.php
@@ -2,67 +2,64 @@
 <html lang="ko-KR">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no, viewport-fit=cover" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
-    <title>KSSO 2024</title>
+	<title>KSSO 2024</title>
 
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="ICOMES 2023">
-    <meta property="og:image" content="/main/img/poster2023.jpg" />
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="ICOMES 2023">
+	<meta property="og:image" content="/main/img/poster2023.jpg" />
 
-    <meta property="og:description" content="2024 KSSO hosted by KSSO">
-    <meta name="description" content="2024 KSSO hosted by KSSO">
+	<meta property="og:description" content="2024 KSSO hosted by KSSO">
+	<meta name="description" content="2024 KSSO hosted by KSSO">
 
-    <!-- datepicker -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
-    <!-- slick -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" href="https://use.typekit.net/uwj4our.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="/main/css/common.css?ver=<?= time() ?>">
-    <link rel="stylesheet" href="/main/css/style.css?ver=<?= time() ?>">
-    <link rel="stylesheet" href="/main/css/style2.css?ver=<?= time() ?>">
-    <link rel="stylesheet" href="/main/css/style3.css?ver=<?= time() ?>">
-    <link rel="stylesheet" href="/main/css/style4.css?ver=<?= time() ?>">
-    <script src="/main/js/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="/main/js/style.js?ver=<?= time() ?>"></script>
-    <script src="/main/js/config.js"></script>
-    <script src="/main/js/common.js?ver=<?= time() ?>"></script>
-    <script src="/main/js/en.js?ver=<?= time() ?>"></script>
-    <script src="/main/js/ko.js?ver=<?= time() ?>"></script>
-    <script src="/main/js/lang.js"></script>
-    <script src="/main/js/jquery.zoomooz.min.js"></script>
-    <script src="/main/js/locale.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y16N5RM5E5"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
+	<!-- datepicker -->
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+	<!-- slick -->
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+	<link rel="stylesheet" href="https://use.typekit.net/uwj4our.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/main/css/common.css?ver=<?= time() ?>">
+	<link rel="stylesheet" href="/main/css/style.css?ver=<?= time() ?>">
+	<link rel="stylesheet" href="/main/css/style2.css?ver=<?= time() ?>">
+	<link rel="stylesheet" href="/main/css/style3.css?ver=<?= time() ?>">
+	<link rel="stylesheet" href="/main/css/style4.css?ver=<?= time() ?>">
+	<script src="/main/js/jquery-3.6.0.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+	<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+	<script src="/main/js/style.js?ver=<?= time() ?>"></script>
+	<script src="/main/js/config.js"></script>
+	<script src="/main/js/common.js?ver=<?= time() ?>"></script>
+	<script src="/main/js/en.js?ver=<?= time() ?>"></script>
+	<script src="/main/js/ko.js?ver=<?= time() ?>"></script>
+	<script src="/main/js/lang.js"></script>
+	<script src="/main/js/jquery.zoomooz.min.js"></script>
+	<script src="/main/js/locale.js"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y16N5RM5E5"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
 
-    gtag('config', 'G-Y16N5RM5E5');
-    </script>
+		gtag('config', 'G-Y16N5RM5E5');
+	</script>
 </head>
 
 <body>
-    <?php
+	<?php
 	if (strpos($_SERVER["PHP_SELF"], "/main/") !== false) {
 		include_once("./common/common.php");
 	} else {
 		include_once("./main/common/common.php");
 	}
-	
+
 	// 230718 HUBDNC 위치 변경
 	$_page_config = array(
 		"m1" => [
@@ -115,11 +112,11 @@
 	//오늘 날짜 구하기 d_day 구하기
 	$today = date("Y. m. d");
 
-	$d_day = new DateTime("2024-03-08");
+	$d_day = new DateTime("2024-03-07");
 
 	$current_date = new DateTime();
 	$current_date->format('Y-m-d');
 
 	$intvl = $current_date->diff($d_day);
-	$d_days = $intvl->days ;
-?>
+	$d_days = $intvl->days;
+	?>
