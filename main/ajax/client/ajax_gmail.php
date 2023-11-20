abd8b1f7f864ab8a37313e665506357b95eb6754
new-crystal <oj3339@naver.com>
2023-11-20 11:20:21 +0900

    edit abstract mail

--- main/ajax/client/ajax_gmail.php
+++ main/ajax/client/ajax_gmail.php
@@ -240,7 +240,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 								<tbody>
 									<tr>
 										<th style='width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;'>ID (Email Address)</th>
-										<td colspan='2' style='font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;'><a href='mailto:{$email}' class='link font_inherit'>{$email}</a></td>
+										<td colspan='2' style='font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;'><a class='link font_inherit'>{$email}</a></td>
 									</tr>
 									{$name_kor_cont}
 									{$affiliation_kor_cont}
@@ -401,7 +401,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 								<tbody>
 									<tr>
 										<th style='width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;'>ID(Email Address)</th>
-										<td style='font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;'><a href='mailto:{$to}' class='link font_inherit'>{$to}</a></td>
+										<td style='font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;'><a  class='link font_inherit'>{$to}</a></td>
 									</tr>
 									<tr>
 										<th style='width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;'>임시 비밀번호</th>
@@ -650,7 +650,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 						<td>
 							<div>
 								<p style='font-size:15px; font-weight:bold; color:#000; margin:0;'>Dear {$fname},</p>
-								<p style='font-size:14px;color:#170F00;margin-top:14px;'>제 58차 대한비만학회 춘계학술대회에 등록해주셔서 감사합니다. 등록 세부 정보는 아래에 기재되어 있습니다. 문의사항이 있으시면 사무국(<a href='mailto:ksso@into-on.com'>ksso@into-on.com</a>)으로 연락해 주시기 바랍니다.</p>
+								<p style='font-size:14px;color:#170F00;margin-top:14px;'>제 58차 대한비만학회 춘계학술대회에 등록해주셔서 감사합니다. 등록 세부 정보는 아래에 기재되어 있습니다. 문의사항이 있으시면 사무국(<a >ksso@into-on.com</a>)으로 연락해 주시기 바랍니다.</p>
 								<table width='586' style='width:586px; border-collapse:collapse; border-top:2px solid #000; width:100%; margin:17px 0;'>
 									<tbody>
 										<tr style='border-bottom:1px solid #000;'>
@@ -949,7 +949,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 
 			$rawMessageString .= "<div style='width:549px;background-color:#fff;border:1px solid #000;'><img src='https://www.kosso.org/main/img/mail_header.png' style='width:100%;margin-bottom:47px;'><h1 style='text-align:center; font-size:16px; font-weight:bold'>Letter of Invitation</h1><div style='width:calc(100% - 80px); margin:24px auto 100px; background-color:#f8f8f8; padding:17px 34px 78px 17px; border-top:2px solid #707070; box-sizing:border-box;'><p style='font-size:12px; font-weight:bold; color:#000; margin:0;'>Dear {$fname},</p><p style='font-size:10px; color:#000 ;margin-top:16px; margin-bottom:25px;'>On behalf of the ICOMES organizing committee, we cordially invite you as participant to “ICOMES 2022 International Conference” to be held at the Conrad Seoul Hotel, Seoul, Korea on September 1(Thu)-3(Sat), 2022. </p><p style='font-size:10px; color:#000; margin-bottom:25px;'>ICOMES has grown as a worldwide academic society with more than 1000 participants and eminent representative speakers in obesity every year since 2015 at its launch. ICOMES is an international academic conference that promotes cooperation among multidisciplinary study fields, providing in-depth lectures and symposiums on basic medicine and clinical medicine on obesity, metabolic syndrome, dyslipidemia, and other obesity-related diseases.<br/>The main theme of ICOMES 2022 is ‘The Next Normal - The Future of Obesity Care’. Your Presentation will be a great addition to our conference.</p><p style='font-size:10px; color:#000;'>For building instructive, insightful and interesting meeting, we would like you to take as a role of; </p><div style='padding:10px 0; margin-top:16px; border-top:1px solid #000; border-bottom:1px solid #000;'><div><span style='vertical-align:middle; width:56px; height:18px; border-radius:14px; border:1px solid #5DBC9B; line-height:16px; display:inline-block; text-align:center; font-size:10px; font-weight:bold; margin-right:7px;'>Date</span><span style='vertical-align:middle; font-size:10px;'>September 1(Thu)~3(Sat)</span></div>
 			<div><span style='vertical-align:middle; width:56px; height:18px; border-radius:14px; border:1px solid #5DBC9B; line-height:16px; display:inline-block; text-align:center; font-size:10px; font-weight:bold; margin-right:7px;'>Venue</span><span style='vertical-align:middle; font-size:10px;'>Conrad Hotel Seoul, Korea</span></div></div><div style='font-size:10px; margin:14px 0 60px;'>
-			We invite you to ICOMES 2022 to create an informative, insightful and exciting<br/>meeting. <a href='mailto:icomes_registration@into-on.com' style='font-size:10px; font-weight:bold; color:#10BF99;'>(icomes_registration@into-on.com).</a></div><ul style='margin:0; padding:0; text-align:center; font-size:0;'><li style='text-align:center; list-style:none; display:inline-block; vertical-align:top;'><p style='font-size:12px; font-weight:bold; margin:0;'>Kijin Kim</p><p style='font-size:8px;'>Chairman of Korean Society<br/>for the Study of Obesity</p><img src='https://kosso.org/main/img/mail_sign01.png' alt=''></li><li style='text-align:center; list-style:none; display:inline-block; vertical-align:top; margin-left:15%;'><p style='font-size:12px; font-weight:bold; margin:0;'>Chang-Beom Lee</p><p style='font-size:8px;'>President of Korean Society<br/>for the Study of Obesity</p><img src='https://kosso.org/main/img/mail_sign02.png' style='margin-top:10px;' alt=''></li></ul></div><img src='https://kosso.org/main/img/mail_footer.png' style='width:100%;'></div>";
+			We invite you to ICOMES 2022 to create an informative, insightful and exciting<br/>meeting. <a style='font-size:10px; font-weight:bold; color:#10BF99;'>(icomes_registration@into-on.com).</a></div><ul style='margin:0; padding:0; text-align:center; font-size:0;'><li style='text-align:center; list-style:none; display:inline-block; vertical-align:top;'><p style='font-size:12px; font-weight:bold; margin:0;'>Kijin Kim</p><p style='font-size:8px;'>Chairman of Korean Society<br/>for the Study of Obesity</p><img src='https://kosso.org/main/img/mail_sign01.png' alt=''></li><li style='text-align:center; list-style:none; display:inline-block; vertical-align:top; margin-left:15%;'><p style='font-size:12px; font-weight:bold; margin:0;'>Chang-Beom Lee</p><p style='font-size:8px;'>President of Korean Society<br/>for the Study of Obesity</p><img src='https://kosso.org/main/img/mail_sign02.png' style='margin-top:10px;' alt=''></li></ul></div><img src='https://kosso.org/main/img/mail_footer.png' style='width:100%;'></div>";
 		}
 
 
@@ -975,38 +975,38 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 			$rawMessageString .= '<div><table width="750" style="border:1px solid #000;padding: 0;">
 								<tr><td colspan="3"><img src="https://www.kosso.org/main/img/mail_header.png" width="750" style="width:100%; max-width:100%;"></td></tr>
 								<tr><td width="74" style="width:74px;"></td><td>
-								<div style="font-weight:bold; text-align:center;font-size: 21px; color: #00666B;padding: 20px 0;">[ICOMES 2023] Completed Abstract Submission</div></td><td width="74" style="width:74px;"></td></tr>
-								<tr><td width="74" style="width:74px;"></td><td><div><p style="font-size:15px; font-weight:bold; color:#000; margin:0;">Dear ' . $first_name . ' ' . $last_name . ',</p><p style="font-size:14px;color:#170F00;margin-top:14px;">Thank you for the online submission of your abstract to ICOMES 2023.<br>Your abstract has been successfully submitted as follows.</p>
+								<div style="font-weight:bold; text-align:center;font-size: 21px; color: #00666B;padding: 20px 0;">[KSSO 2023] 초록 제출이 완료되었습니다.</div></td><td width="74" style="width:74px;"></td></tr>
+								<tr><td width="74" style="width:74px;"></td><td><div><p style="font-size:15px; font-weight:bold; color:#000; margin:0;">Dear ' . $first_name . ' ' . $last_name . ',</p><p style="font-size:14px;color:#170F00;margin-top:14px;">대한비만학회 춘계학술대회에 초록 제출 감사합니다.<br>귀하의 초록은 다음과 같이 성공적으로 제출되었습니다.</p>
 								<!-- Abstract Submission Status -->
-								<p style="font-size:17px; font-weight:bold; color:#000;  margin: 30px 0 0;">Abstract Submission Status</p>
+								<p style="font-size:17px; font-weight:bold; color:#000;  margin: 30px 0 0;">초록 제출 상태</p>
 								<table width="586" style="width:586px; border-collapse:collapse; border-top:2px solid #000; width:100%; margin:10px 0 30px;">
 								<tbody>
 								<tr>
-									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Submission No.</th>
+									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">초록 접수 번호</th>
 									<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;">' . $submission_code . '</td>
 								</tr>
 								<tr>
-									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Type of Presentation</th>
+									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">발표 형식</th>
 									<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="2">' . ($presentation_type + 1) . '. ' . $presentation_type_arr[$presentation_type] . '</td>
 								</tr>
 								<tr>
-									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Topic Category</th>
+									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">주제 카테고리</th>
 									<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="2">' . $poster_category_map[$abstract_category] . '</td>
 								</tr>
 								<tr>
-									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Abstract Title</th>
+									<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">초록 제목</th>
 									<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000; white-space:nowrap;" colspan="2">' . $abstract_title . '</td>
 								</tr>
 								</tbody>
 								</table>';
 
 			if (count($presenting_author_data) > 0 || count($corresponding_submit_data) > 0) {
-				$rawMessageString .= '<p style="font-size:17px; font-weight:bold; color:#000; margin: 20px 0 10px;">Abstract Submission Status</p>';
+				$rawMessageString .= '<p style="font-size:17px; font-weight:bold; color:#000; margin: 20px 0 10px;">초록 접수 상태</p>';
 			}
 
 			if (count($presenting_author_data) > 0) {
 				$rawMessageString .= '<!-- Abstract Submission Status > Presenting Author -->
-								<p style="font-size:15px; font-weight:600; color:#000; margin:0;">Presenting Author</p>';
+								<p style="font-size:15px; font-weight:600; color:#000; margin:0;">발표자</p>';
 
 				foreach ($presenting_author_data as $obj) {
 					$nation_no			= $obj["nation_no"] ?? "";
@@ -1030,21 +1030,21 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 					$rawMessageString .= '<table width="586" style="width:586px; border-collapse:collapse; border-top:2px solid #000; width:100%; margin:10px 0 20px;">
 										<tbody>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Name</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">성함</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; width:165px; border-bottom:1px solid #000;">' . $first_name . ' ' . $last_name . '</td>
-											<th style="width:150px; text-align:left; border-left:1px solid #000; font-size:14px; padding:10px; border-bottom:1px solid #000;">Country</th>
+											<th style="width:150px; text-align:left; border-left:1px solid #000; font-size:14px; padding:10px; border-bottom:1px solid #000;">국가</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; width:165px; border-bottom:1px solid #000;">' . $nation_map[$nation_no] . '</td>
 										</tr>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Affiliation</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">소속</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3">' . (implode("<br>", $affiliation_arr)) . '</td>
 										</tr>
 										<tr>
 											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">E-mail</th>
-											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3"><a href="mailto:' . $email . '">' . $email . '</a></td>
+											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3"><a >' . $email . '</a></td>
 										</tr>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Phone Number</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">휴대폰 번호</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3">(+' . $nation_tel . ')' . $phone . '</td>
 										</tr>
 										</tbody>	
@@ -1078,21 +1078,21 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 					$rawMessageString .= '<table width="586" style="width:586px; border-collapse:collapse; border-top:2px solid #000; width:100%; margin:10px 0 20px;">
 										<tbody>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Name</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">성함</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; width:165px; border-bottom:1px solid #000;">' . $first_name . ' ' . $last_name . '</td>
-											<th style="width:150px; text-align:left; border-left:1px solid #000; font-size:14px; padding:10px; border-bottom:1px solid #000;">Country</th>
+											<th style="width:150px; text-align:left; border-left:1px solid #000; font-size:14px; padding:10px; border-bottom:1px solid #000;">국가</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; width:165px; border-bottom:1px solid #000;">' . $nation_map[$nation_no] . '</td>
 										</tr>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Affiliation</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">소속</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3">' . (implode("<br>", $affiliation_arr)) . '</td>
 										</tr>
 										<tr>
 											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">E-mail</th>
-											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3"><a href="mailto:' . $email . '">' . $email . '</a></td>
+											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3"><a>' . $email . '</a></td>
 										</tr>
 										<tr>
-											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">Phone Number</th>
+											<th style="width:150px; text-align:left; font-size:14px; padding:10px; border-bottom:1px solid #000;">휴대폰 번호</th>
 											<td style="font-size:14px; padding:10px; border-left:1px solid #000; border-bottom:1px solid #000;" colspan="3">(+' . $nation_tel . ')' . $phone . '</td>
 										</tr>
 										</tbody>	
@@ -1100,7 +1100,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 				}
 			}
 
-			$rawMessageString .=  '<p style="margin: 0 34px 10px 0">If you have any questions regarding abstract submission, please contact the secretariat.(<a href="mailto:icomes_abstracts@into-on.com">icomes_abstracts@into-on.com</a>) We look forward to seeing you in ICOMES 2023</p>
+			$rawMessageString .=  '<p style="margin: 0 34px 10px 0">만약 초록 제출과 관련된 문의사항이 있으시면, 사무국으로 연락 부탁드립니다.(<a href="mailto:ksso@into-on.com">ksso@into-on.com</a>)</p>
 								</div>
 								</td>
 								<td width="74" style="width:74px;"></td>
@@ -1109,11 +1109,10 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 									<td width="74" style="width:74px;"></td>
 									<td>
 										<!-- 23.04.25 수정된 버튼 마크업 -->
-										<p>Best regards,</p>
-										<p>Secretariat of ICOMES 2023</p><br/>
+										<p>항상 행운을 빕니다.</p>
 										<div style="text-align: center;">
 											<a href="https://kosso.org/">
-												<img src="https://kosso.org/main/img/ksso_btn.png" alt="">
+												<img src="https://www.kosso.org/main/img/ksso_btn.png" alt="">
 											</a>
 										</div>	
 									</td>
@@ -1121,7 +1120,7 @@ function createMessage($language, $mail_type, $fname, $to, $subject, $time, $tmp
 								</tr>
 								<tr>
 									<td style="padding-top:50px;" colspan="3">
-										<img src="https://kosso.org/main/img/mail_footer.png" width="750" style="width:100%; max-width:100%;">
+										<img src="https://www.kosso.org/main/img/mail_footer.png" width="750" style="width:100%; max-width:100%;">
 									</td>
 								</tr>
 								</table>
@@ -1375,7 +1374,12 @@ if ($_POST["flag"] == "find_password") {
 		"nation_map"				=> $nation_map
 	];
 
-	$message = createMessage($language, "abstract", "", $email, "[KSSO 2024]" . $subject, date("Y-m-d H:i:s"), "", "", 1, "", "", "", $user_email, date("Y-m-d H:i:s"), $title, $abstract_title, $data);
+	/** 1120 메일 제목을 받아오는 경우 보내지지 않음
+	 * 메일 제목 직접 입력으로 수정
+	 */
+	//$subject = "초록 접수 메일 안내";
+
+	$message = createMessage($language, "abstract", "", $email, "[KSSO 2024]초록 접수 메일 안내", date("Y-m-d H:i:s"), "", "", 1, "", "", "", $user_email, date("Y-m-d H:i:s"), $title, $abstract_title, $data);
 	createDraft($service, "secretariat@kosso.org", $message);
 	sendMessage($service, "secretariat@kosso.org", $message);
 }
