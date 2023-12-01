d9df88c3b2fcbbbe073b8a908873e71dc48b9cc5
new-crystal <oj3339@naver.com>
2023-12-01 09:28:13 +0900

    edit header korean, footer 3

--- main/include/footer.php
+++ main/include/footer.php
@@ -13,8 +13,9 @@
             if ($_SESSION["USER"]["regi_status"] == 2 || $_SESSION["USER"]["regi_status"] == 5) {
             ?>
                 <!--[230824] 다운로드 버튼 추가 / 파일 전달X-->
-                <!-- [231116]다운로드 버튼 막기 -->
-                <li>
+                <!-- [231116]다운로드 버튼 막기 
+                     [231201] 학회팀 요청 주석-->
+                <!-- <li>
                     <a href="" class="type2 pink not_yet">
                         <i><img src="/main/img/icons/icon_download_abstract.svg" alt=""></i>
                         Abstract Book <br />Download
@@ -25,12 +26,36 @@
                         <i><img src="/main/img/icons/icon_download_program.svg" alt=""></i>
                         Program Book <br />Download
                     </a>
+                </li> -->
+                <li>
+                    <button type="button" class="" onclick="goAbstract()">
+                        <img src="/main/img/icons/tool_abstract.svg" alt="">
+                    </button>
                 </li>
-
 
             <?php
             } else {
             ?>
+                <!-- <li>
+                    <button type="button" class="" onclick="goRegistration()">
+                        <img src="/main/img/icons/tool_regist.svg" alt="">
+                    </button>
+                </li>
+                <li>
+                    <button type="button" class="" onclick="goAbstract()">
+                        <img src="/main/img/icons/tool_abstract.svg" alt="">
+                    </button>
+                </li> -->
+            <?php
+            }
+            ?>
+            <?php
+            if ($_SESSION["USER"]["idx"] == "") {
+            ?>
+            <!-- 1201 학회팀 요청으로 
+                 로그인 전 -> 프로그램, 사전등록, 초록접수
+                로그인 후 => 프로그램, 초록접수, 마이페이지
+            -->
                 <li>
                     <button type="button" class="" onclick="goRegistration()">
                         <img src="/main/img/icons/tool_regist.svg" alt="">
@@ -41,16 +66,17 @@
                         <img src="/main/img/icons/tool_abstract.svg" alt="">
                     </button>
                 </li>
-            <?php
-            }
-            ?>
-            <?php
-            if ($_SESSION["USER"]["idx"] == "") {
-            ?>
-                <li><button type="button" onClick="alert('Need to login.')"><img src="/main/img/icons/tool_mypage.svg" alt=""></button></li>
+                <li>
+                    <!-- <button type="button" onClick="alert('Need to login.')"><img src="/main/img/icons/tool_mypage.svg" alt=""></button> -->
+                </li>
             <?php
             } else {
             ?>
+                 <li>
+                    <button type="button" class="" onclick="goAbstract()">
+                        <img src="/main/img/icons/tool_abstract.svg" alt="">
+                    </button>
+                </li>
                 <li><button type="button" onClick="location.href='/main/mypage.php'"><img src="/main/img/icons/tool_mypage.svg" alt=""></button></li>
             <?php
             }
